a342ffd614eb207ea04e820ba8cfd57d9fae33f3
LLDB <120081647+SSL-ACTX@users.noreply.github.com>
2022-12-15 12:28:34 +0800

    Create test-time.go

---
+++ Go/test-time.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func main() {
+  
+  currentTime := time.Now()
+	fmt.Println("The time is", currentTime)
+	fmt.Println("The year is", currentTime.Year())
+	fmt.Println("The month is", currentTime.Month())
+	fmt.Println("The day is", currentTime.Day())
+	fmt.Println("The hour is", currentTime.Hour())
+	fmt.Println("The minute is", currentTime.Hour())
+	fmt.Println("The second is", currentTime.Second())
+  
+}
+
+// Time
